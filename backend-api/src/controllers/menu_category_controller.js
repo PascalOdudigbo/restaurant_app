27146d79b62e0b9a486bdd69cf8558f3fc0e646b
Pascal Odudigbo <odudigbopascal@gmail.com>
2024-02-26 16:57:50 +0000

    creating the menu category controller and implementing it's basic functions

---
+++ backend-api/src/controllers/menu_category_controller.js
@@ -0,0 +1,68 @@
+// Importing the menu category resources 
+const { listAll, getById, save, update, destroy } = require("../resources/menu_category_resources");
+
+// A controller function to get all menu categories
+const getAllMenuCategories = (req, res) => {
+    try {
+        // Call the listAllMenuCategories function from the resource file
+        listAll(req, res);
+    } catch (error) {
+        // In the eventuality of an error occurring
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+// A controller function to get a menu category by ID
+const getMenuCategoryById = (req, res) => {
+    try {
+        // Call the getMenuCategoryById function from the resource file
+        getById(req, res);
+    } catch (error) {
+        // In the eventuality of an error occurring
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+// A controller function to add a menu category to the database
+const addMenuCategory = (req, res) => {
+    try {
+        // Call the addMenuCategory function from the resource file
+        save(req, res);
+    } catch (error) {
+        // In the eventuality of an error occurring
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+// A controller function to update menu category
+const updateMenuCategory = (req, res) => {
+    try {
+        // Call the updateMenuCategory function from the resource file
+        update(req, res);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+// A controller function to delete menu category
+const deleteMenuCategory = (req, res) => {
+    try {
+        // Call the deleteMenuCategory function from the resource file
+        destroy(req, res);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+module.exports = {
+    getAllMenuCategories,
+    getMenuCategoryById,
+    addMenuCategory,
+    updateMenuCategory,
+    deleteMenuCategory
+}
