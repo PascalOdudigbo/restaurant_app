756906bb1e94e340526d8aaf7c2e47db4dd584d4
Pascal Odudigbo <odudigbopascal@gmail.com>
2024-02-26 19:38:01 +0000

    creating the table routes

--- backend-api/src/routes/menu_item_routes.js
+++ backend-api/src/routes/menu_item_routes.js
@@ -2,19 +2,19 @@ const { Router } = require('express')
 
 const router = Router();
 
-// Importing the menu categories controller functions
+// Importing the menu items controller functions
 const {getAllMenuItems, getMenuItemById, addMenuItem, updateMenuItem, deleteMenuItem} = require("../controllers/menu_item_controller");
 
-// A route to get all the menu categorys
+// A route to get all the menu items
 router.get("/", getAllMenuItems);
 
-// A router to get menu category by ID
+// A router to get menu item by ID
 router.get("/:id", getMenuItemById);
 
-// A router to add a menu category to the database
+// A router to add a menu item to the database
 router.post("/", addMenuItem);
 
-// Router to update menu category
+// Router to update menu item
 router.patch("/:id", updateMenuItem);
 router.put("/:id", updateMenuItem);
 
