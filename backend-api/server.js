1c004bc57d5a474c6bf9663ac165928f9ca06838
Pascal Odudigbo <odudigbopascal@gmail.com>
2024-02-26 19:49:10 +0000

    importing and making use of all the routes in the server file

--- backend-api/server.js
+++ backend-api/server.js
@@ -7,6 +7,15 @@ const client = require("./db");
 // Importing all the route files
 const userRoutes = require('./src/routes/user_routes');
 const bookingRoutes = require('./src/routes/booking_routes');
+const menuCategoryRoutes = require('./src/routes/menu_category_routes');
+const menuItemRoutes = require('./src/routes/menu_item_routes');
+const tableRoutes = require('./src/routes/table_routes');
+const orderRoutes = require('./src/routes/order_routes');
+const orderItemRoutes = require('./src/routes/order_item_routes');
+
+
+
+
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -29,6 +38,21 @@ app.use('/users', userRoutes);
 
 // Creating an API route for bookings that links to the booking routes
 app.use('/bookings', bookingRoutes);
+
+// Creating an API route for menu categories that links to the menu category routes
+app.use('/bookings', menuCategoryRoutes);
+
+// Creating an API route for menu items that links to the menu item routes
+app.use('/bookings', menuItemRoutes);
+
+// Creating an API route for tables that links to the table routes
+app.use('/bookings', tableRoutes);
+
+// Creating an API route for orders that links to the order routes
+app.use('/bookings', orderRoutes);
+
+// Creating an API route for order items that links to the order items routes
+app.use('/bookings', orderItemRoutes);
 
 // Start the Express server
 app.listen(port, () => {
