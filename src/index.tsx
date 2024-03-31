e3613f258b1d85c85939c2781aca37f5c7720006
Pascal Odudigbo <odudigbopascal@gmail.com>
2024-03-31 14:23:50 +0100

    disabling react dev tools in production mode

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './scss/_index.scss';
 import { BrowserRouter } from 'react-router-dom';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
